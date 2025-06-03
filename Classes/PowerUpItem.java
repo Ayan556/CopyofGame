1c534a925ffb7d2be124bace97505339c262e3ee
Ayan Talukdar <63212194+Ayan556@users.noreply.github.com>
2025-06-03 18:49:06 -0400

    Display and activate queued power-ups

---
+++ Classes/PowerUpItem.java
@@ -0,0 +1,30 @@
+public class PowerUpItem extends java.awt.Rectangle {
+    private PowerUp powerUp;
+    private java.awt.image.BufferedImage image;
+    private java.awt.Color color;
+
+    public PowerUpItem(int x, int y, int size, PowerUp powerUp,
+                       java.awt.image.BufferedImage image, java.awt.Color color) {
+        super(x, y, size, size);
+        this.powerUp = powerUp;
+        this.image = image;
+        this.color = color;
+    }
+
+    public PowerUp getPowerUp() {
+        return powerUp;
+    }
+
+    public java.awt.image.BufferedImage getImage() {
+        return image;
+    }
+
+    public void draw(java.awt.Graphics2D g2, int xOffset, int yOffset) {
+        if (image != null) {
+            g2.drawImage(image, this.x + xOffset, this.y + yOffset, this.width, this.height, null);
+        } else {
+            g2.setColor(color);
+            g2.fillRect(this.x + xOffset, this.y + yOffset, this.width, this.height);
+        }
+    }
+}
