470dc4ff1148a330f35bce1fdecad03b9e804976
Ayan Talukdar <63212194+Ayan556@users.noreply.github.com>
2025-06-12 01:28:03 -0400

    Copy resource assets into build

--- compile.sh
+++ compile.sh
@@ -2,3 +2,8 @@
 # Compile all Java sources with Java 21
 mkdir -p out
 javac --release 21 -d out Classes/*.java
+
+# Copy resources so they are on the runtime classpath
+cp -r res/Audio out/
+cp -r res/Fonts out/
+cp -r res/images out/
