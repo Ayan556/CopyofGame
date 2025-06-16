0391e686b78a525c34b88c09643edbebe2a4a9cd
Ayan Talukdar <inayantalukdar@gmail.com>
2025-06-16 12:48:07 -0400

    Update from original

--- Classes/Main.java
+++ Classes/Main.java
@@ -397,7 +397,7 @@ public class Main extends JPanel implements ActionListener, KeyListener {
 			paused = true;
 			repaint();
 			return;
-		} else if (e.getKeyCode() == KeyEvent.VK_L && paused && resume) {
+		} else if ((e.getKeyCode() == KeyEvent.VK_L || e.getKeyCode() == KeyEvent.VK_U || e.getKeyCode() == KeyEvent.VK_J)&& paused && resume) {
 			paused = false;
 			if (waveInProgress) {
 				timer.start();
@@ -405,7 +405,7 @@ public class Main extends JPanel implements ActionListener, KeyListener {
 			SoundPlayer.resumeBackground();
 			repaint();
 			return;
-		} else if (e.getKeyCode() == KeyEvent.VK_L && paused) {
+		} else if ((e.getKeyCode() == KeyEvent.VK_L || e.getKeyCode() == KeyEvent.VK_U || e.getKeyCode() == KeyEvent.VK_J) && paused) {
 			SoundPlayer.stopBackground();
 			switchScreens.showHomepage();
 			return;
